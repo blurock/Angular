c23f04509c064dbfac8d4f1ed0cb3cc4b44cc8c3
edwardblurock <edward.blurock@gmail.com>
2021-08-02 11:13:10 +0200

    Add comments to CreateHierarchyElement

--- ChemConnectCoreOntologyBase/src/main/java/info/esblurock/reaction/core/ontology/base/hierarchy/CreateHierarchyElement.java
+++ ChemConnectCoreOntologyBase/src/main/java/info/esblurock/reaction/core/ontology/base/hierarchy/CreateHierarchyElement.java
@@ -13,11 +13,37 @@ import info.esblurock.reaction.core.ontology.base.constants.OntologyObjectLabels
 import info.esblurock.reaction.core.ontology.base.dataset.DatasetOntologyParseBase;
 import info.esblurock.reaction.core.ontology.base.utilities.OntologyUtilityRoutines;
 
+/**
+ * @author edwardblurock
+ * 
+ * This class generates the FirestoreCatalogID as an array of CollectionDocumentIDPair
+ * The elements of the CollectionDocumentIDPair are generated as specified by
+ * the subclass of the CollectionDocumentHierarchy:
+ * rdfs:isDefinedBy: How the label is to be derived
+ * dc:type: Which element in CollectionDocumentIDPair to fill in
+ * 
+ * The main routine: {@link searchForCatalogObjectInHierarchyTemplate}
+ *
+ */
 public class CreateHierarchyElement {
 
 	static String topOfHierarchy = "dataset:CollectionDocumentHierarchy";
 	static String simpleName = "dataset:DatasetCatalogHierarchySimpleName";
+	static String datacatalog = "dataset:DatasetCatalogHierarchyDataCatalog";
+	static String collection = "dataset:DatasetCatalogHierarchyCollection";
+	static String document = "dataset:DatasetCatalogHierarchyDocument";
 
+	/** Generate the FirestoreCatalogID from the class
+	 * 
+	 * The classname and the catalog object is used to generate the FirestoreCatalogID.
+	 * The catalog object and classname elements may be used in deteriming the document 
+	 * and collection names of the firestore hierarchy.
+	 * 
+	 * @param catalogC The classname of the catalog object
+	 * @param json The catalog object
+	 * 
+	 * @return An array of CollectionDocumentIDPair (FirestoreCatalogID)
+	 */
 	public static JsonArray searchForCatalogObjectInHierarchyTemplate(String catalogC, JsonObject json) {
 		JsonArray pairs = new JsonArray();
 		JsonObject pair = initialCollectionDocumentIDPair();
@@ -26,6 +52,15 @@ public class CreateHierarchyElement {
 		return pairs;
 	}
 
+	/** Search down the classification hierarchy and fill in FirestoreCatalogID
+	 * 
+	 * @param hierarchy The classification hierarchy starting with dataset:CollectionDocumentHierarchy
+	 * @param json The catalog object in json form
+	 * @param pairs The current array of CollectionDocumentIDPair (FirestoreCatalogID)
+	 * @param pair the CollectionDocumentIDPair being built
+	 * @param catalogC The classname of the catalog object
+	 * @return true if the end point of the hierarch y has been found and the FirestoreCatalogID is being filled in
+	 */
 	private static boolean search(ClassificationHierarchy hierarchy, JsonObject json, JsonArray pairs, JsonObject pair,
 			String catalogC) {
 		boolean foundB = false;
@@ -47,32 +82,58 @@ public class CreateHierarchyElement {
 				foundB = search(hier, json, pairs, pair, catalogC);
 			}
 			if (foundB) {
-				if (!hierarchy.getClassification().equals("dataset:CollectionDocumentHierarchy")) {
+				if (!hierarchy.getClassification().equals(topOfHierarchy)) {
 					String genname = generateHierarchyName(hierarchy.getClassification(), catalogC, json);
 					pair = UpdateHierarchyList(hierarchy.getClassification(), genname, pair, pairs);
 				}
 			}
 		}
-		if (hierarchy.getClassification().equals("dataset:CollectionDocumentHierarchy")) {
+		if (hierarchy.getClassification().equals(topOfHierarchy)) {
 			addPairToArray(pair, pairs);
 		}
 		return foundB;
 	}
 
+	/**
+	 * @param hierclass the current hierarchy class name
+	 * @param genname The generated name (as given by the hierarchy class)
+	 * @param json The current CollectionDocumentIDPair being filled in
+	 * @param pairs The current array of CollectionDocumentIDPair (FirestoreCatalogID)
+	 * @return The new CollectionDocumentIDPair 
+	 * 
+	 * From the hierarchy class, what name is to be filled in (document or collection) is determined
+	 * This determined the position in the CollectionDocumentIDPair 
+	 * addInCollectionDocumentIDPair determines how the pair is to be updated.
+	 * 
+	 */
 	public static JsonObject UpdateHierarchyList(String hierclass, String genname, JsonObject json, JsonArray pairs) {
 		String type = DatasetOntologyParseBase.getValueFromAnnotation(hierclass, OntologyObjectLabels.dctype);
-		if (type.equals("dataset:DatasetCatalogHierarchySimpleName")) {
+		if (type.equals(simpleName)) {
 			json = addInCollectionDocumentIDPair(ClassLabelConstants.DatasetDocumentID, genname, json, pairs);
-		} else if (type.equals("dataset:DatasetCatalogHierarchyDataCatalog")) {
+		} else if (type.equals(datacatalog)) {
 			json = addInCollectionDocumentIDPair(ClassLabelConstants.DatasetCollectionID, genname, json, pairs);
-		} else if (type.equals("dataset:DatasetCatalogHierarchyCollection")) {
+		} else if (type.equals(collection)) {
 			json = addInCollectionDocumentIDPair(ClassLabelConstants.DatasetCollectionID, genname, json, pairs);
-		} else if (type.equals("dataset:DatasetCatalogHierarchyDocument")) {
+		} else if (type.equals(document)) {
 			json = addInCollectionDocumentIDPair(ClassLabelConstants.DatasetDocumentID, genname, json, pairs);
 		}
 		return json;
 	}
 
+	/** Fill in the CollectionDocumentIDPair
+	 * 
+	 * @param genname The generated name
+	 * @param identifier The position in CollectionDocumentIDPair
+	 * @param json the current CollectionDocumentIDPair
+	 * @param pairs The current array of CollectionDocumentIDPair (FirestoreCatalogID)
+	 * @return The 'new' current CollectionDocumentIDPair
+	 * 
+	 * This fills in the CollectionDocumentIDPair
+	 * If the element pointed to by the identifier is non-null, this means
+	 * that the CollectionDocumentIDPair is full and should be added to the FirestoreCatalogID.
+	 * if this is true, then a new (empty) CollectionDocumentIDPair is generated and filled in.
+	 * 
+	 */
 	private static JsonObject addInCollectionDocumentIDPair(String genname, String identifier, JsonObject json,
 			JsonArray pairs) {
 		if (json.get(identifier) == null) {
@@ -86,6 +147,10 @@ public class CreateHierarchyElement {
 		return json;
 	}
 
+	/** Add a CollectionDocumentIDPair to the FirestoreCatalogID and update the levels
+	 * @param json The CollectionDocumentIDPair to add
+	 * @param pairs The FirestoreCatalogID
+	 */
 	private static void addPairToArray(JsonObject json, JsonArray pairs) {
 		pairs.add(json);
 		for (JsonElement ele : pairs) {
@@ -96,13 +161,17 @@ public class CreateHierarchyElement {
 		}
 	}
 
+	/** Create an empty CollectionDocumentIDPair (level is set to zero).
+	 * @return An initial CollectionDocumentIDPair
+	 */
 	private static JsonObject initialCollectionDocumentIDPair() {
 		JsonObject json = new JsonObject();
 		json.addProperty(ClassLabelConstants.DatasetIDLevel, 0);
 		return json;
 	}
 
-	/**
+	/** Generate the Hierarchy name
+	 * 
 	 * Derive a name given the hierarchy class name
 	 * 
 	 * @param hierclass The hierarchy class name
